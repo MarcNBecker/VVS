78cb0c29cc61919e06682f5605e34cccd83b1c7e
MarcNBecker <me@marcnbecker.com>
2015-03-01 03:52:41 +0100

    integrated dozent into home dozent can now trigger reloads and changing the dozentID publish property forces a reload aswell added comments and did some restructuring

--- VVS/WebContent/app/js/home.js
+++ VVS/WebContent/app/js/home.js
@@ -5,51 +5,57 @@ var DEFAULT_ROUTE = 'stammdaten'; //TODO dashboard
 
 var template = document.querySelector('#home');
 
-template.homePages = [
-	{name: 'Stammdaten', hash: 'stammdaten', html: '<vvs-uebersicht></vvs-uebersicht>', loaded: false},
-	{name: 'Link1', hash: 'link1', html: null, loaded: true},
-	{name: 'Link2', hash: 'link2', html: null, loaded: true},
-	{name: 'Link3', hash: 'link3', html: null, loaded: true},
-	{name: 'Link4', hash: 'link4', html: null, loaded: true},
-	{name: 'Link5', hash: 'link5', html: null, loaded: true},
-	{name: 'Link6', hash: 'link6', html: null, loaded: true},
-	{name: 'Link7', hash: 'link7', html: null, loaded: true}
-];
+//page parameter are used with data binding to exchange data between home and the vvs polymer elements
+template.pageParameter = {
+	dozent: {
+		id: 0
+	}
+};
 
-function handleHashChange(reload) {
+//Page descriptors inclduing the HTML code to load the page
+template.pageDescriptor = {
+	stammdaten: {name: 'Stammdaten', hash: 'stammdaten', html: '<vvs-uebersicht></vvs-uebersicht>', loaded: false},
+	dozent: {name: 'Dozent', hash: 'dozent', html: '<vvs-dozent dozentID="{{pageParameter.dozent.id}}" refresh="{{refresh}}" loaded="{{pageDescriptor.dozent.loaded}}"></vvs-dozent>', loaded: false},
+	link2: {name: 'Link2', hash: 'link2', html: null, loaded: true}
+};
+
+//Pages to display in the navigation drawer
+template.homePages = [template.pageDescriptor.stammdaten, template.pageDescriptor.dozent, template.pageDescriptor.link2];
+
+//Is called whenever the url hash is changed and navigates the app
+function handleHashChange(refresh) {
 	template.route = location.hash.substring(1, location.hash.length);
-	if (!template.route) {
+	if (!template.route) { //No hash exists
 		template.route = DEFAULT_ROUTE;
 	}
 	var nextPage = findPage(template.route);
-	if (reload === true) {
+	if (refresh === true) { //Forced refresh?
 		nextPage.loaded = false;
 	}
-	if(!nextPage.loaded) {
+	if(!nextPage.loaded) { //Inject vvs polymer element to correct page
 		nextPage.loaded = true;
 		template.injectBoundHTML(nextPage.html, document.querySelector('#home-pages-'+nextPage.hash));
 	}
 }
 
+//Finds a page or returns a dummy page object
 function findPage(hash) {
-	var i;
-	for (i=0; i<template.homePages.length; i++) {
-		if (template.homePages[i].hash === hash) {
-			return template.homePages[i];
-		}
+	var page = template.pageDescriptor[hash];
+	if(page) {
+		return page;
+	} else {
+		return {name:'', hash: '', html:'', loaded: true};	
 	}
-	return {name:'', hash: '', html:'', loaded: true};
 }
 
 window.addEventListener('hashchange', handleHashChange);
 
 template.addEventListener('template-bound', function(e) {
-	if(location.hash) {
-		handleHashChange();
+	if(location.hash) { //Hash already set
+		handleHashChange(); 
 	} else {
 		this.route = DEFAULT_ROUTE;
-		
-		location.hash = '#' + DEFAULT_ROUTE;
+		location.hash = '#' + DEFAULT_ROUTE; //Update hash manually
 	}
 });
 
@@ -59,12 +65,14 @@ template.menuItemSelected = function(e, detail, sender) {
 	}
 };
 
+//Update hash
 template.navigateFromDrawer = function(e, detail, sender) {
   	var selectedPage = e.target.templateInstance.model.page;
 	location.href = '#' + selectedPage.hash;
 };
 
-template.reload = function(e, detail, sender) {
+//Force a refresh for the current page
+template.refresh = function(e, detail, sender) {
 	handleHashChange(true);
 };
 
