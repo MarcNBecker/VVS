8fae793b8ded17fd5c65d5fda3408d2f06363525
maximilianigers <maximilianigers@gmail.com>
2015-03-10 16:31:52 +0100

    vvs-planung hinzugefügt

--- VVS/WebContent/app/js/home.js
+++ VVS/WebContent/app/js/home.js
@@ -42,7 +42,8 @@ template.pageLoaded = {
 	modulplanPflegen: false,
 	kurs: false,
 	user: false,
-	feiertage: false
+	feiertage: false,
+	planung: false
 };
 
 //Page descriptors including the HTML code to load the page
@@ -53,14 +54,15 @@ template.pageDescriptor = {
 	modulplanPflegen: {name: 'Modulplan pflegen', hash: 'modulplanPflegen', html: '<vvs-modulplan-pflegen pageParameter="{{pageParameter}}" pageLoaded="{{pageLoaded}}" toasts="{{toasts}}"></vvs-modulplan-pflegen>'},
 	kurs: {name: 'Kurs pflegen', hash: 'kurs', html: '<vvs-kurs pageParameter="{{pageParameter}}" pageLoaded="{{pageLoaded}}" toasts="{{toasts}}"></vvs-kurs>'},
 	user: {name: 'User pflegen', hash: 'user', html: '<vvs-user currentUser="{{user}}" pageParameter="{{pageParameter}}" pageLoaded="{{pageLoaded}}" toasts="{{toasts}}"></vvs-user>'},
-	feiertage: {name: 'Feiertage pflegen', hash: 'feiertage', html: '<vvs-feiertage pageParameter="{{pageParameter}}" pageLoaded="{{pageLoaded}}" toasts="{{toasts}}"></vvs-feiertage>'}
+	feiertage: {name: 'Feiertage pflegen', hash: 'feiertage', html: '<vvs-feiertage pageParameter="{{pageParameter}}" pageLoaded="{{pageLoaded}}" toasts="{{toasts}}"></vvs-feiertage>'},
+	planung: {name: 'Planung', hash: 'planung', html: '<vvs-planung></vvs-planung>'}
 };
 
 //Pages to display in the navigation drawer
-template.drawerPages = [template.pageDescriptor.stammdaten];
+template.drawerPages = [template.pageDescriptor.stammdaten, template.pageDescriptor.planung];
 
 //Pages that can be displayed in the home main div
-template.homePages = [template.pageDescriptor.stammdaten, template.pageDescriptor.dozent, template.pageDescriptor.kurs, template.pageDescriptor.modulplanAnlegen, template.pageDescriptor.modulplanPflegen, template.pageDescriptor.user, template.pageDescriptor.feiertage];
+template.homePages = [template.pageDescriptor.stammdaten, template.pageDescriptor.dozent, template.pageDescriptor.kurs, template.pageDescriptor.modulplanAnlegen, template.pageDescriptor.modulplanPflegen, template.pageDescriptor.user, template.pageDescriptor.feiertage, template.pageDescriptor.planung];
 
 //Is called whenever the url hash is changed and navigates the app
 function handleHashChange(refresh) {
